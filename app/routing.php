ea3685b03e346c67f336339d3818aabb94778ae0
Patricia Batjom <patriciabatjom@gmail.com>
2018-10-17 16:31:03 +0200

    creation de la page on est la

--- app/routing.php
+++ app/routing.php
@@ -17,4 +17,7 @@ $routes = [
         ['show', '/item/{id:\d+}', 'GET'], // action, url, method
         ['delete', '/item/delete/{id:\d+}', 'GET'], // action, url, method
     ],
+    'Location' => [ // Controller
+        ['index', '/location', 'GET'], // action, url, method
+    ],
 ];
