312dab1cb3acafcfa0b0578d24124d6b8ad3b706
Jovanela Daribo <carly972@live.fr>
2018-10-19 14:03:58 +0200

    Modif index.php qui n'aurait pas du être là.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * Created by PhpStorm.
@@ -6,10 +5,14 @@
  * Date: 11/10/17
  * Time: 14:01
  */
+
+
 if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
     return false;    // retourne la requête telle quelle.
 }
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/db.php';
 require_once __DIR__ . '/../app/config.php';
 require_once __DIR__ . '/../app/dispatcher.php';
+
