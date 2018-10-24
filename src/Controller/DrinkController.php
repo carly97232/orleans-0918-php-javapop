cdd4c2a11e0e2a1a217bf6ab4534b6a07b2c2694
Melissa Viltet <melissaviltet@gmail.com>
2018-10-24 17:48:46 +0200

    Jarvis3

--- src/Controller/DrinkController.php
+++ src/Controller/DrinkController.php
@@ -12,30 +12,28 @@ namespace Controller;
     use Model\Drink;
     use Model\DrinkManager;
 
-
     /**
      * Class DrinkController
      * @package Controller
      */
     class DrinkController extends AbstractController
-{
-
-        /**
-         * @return string
-         * @throws \Twig_Error_Loader
-         * @throws \Twig_Error_Runtime
-         * @throws \Twig_Error_Syntax
-         */
-        public function index()
     {
-        $drinkManager = new DrinkManager($this->getPdo());
-        $drinks = $drinkManager->selectAllDrink();
+            /**
+             * @return string
+             * @throws \Twig_Error_Loader
+             * @throws \Twig_Error_Runtime
+             * @throws \Twig_Error_Syntax
+             */
+            public function index()
+        {
+            $drinkManager = new DrinkManager($this->getPdo());
+            $drinks = $drinkManager->selectAllDrink();
 
-        $drinksByType= [] ;
-        foreach ($drinks as $drink) {
-            $type_name = str_replace(' ', '', $drink['type_name']);
-            $drinksByType[$type_name][]= $drink;
+            $drinksByType= [] ;
+            foreach ($drinks as $drink) {
+                $type_name = str_replace(' ', '', $drink['type_name']);
+                $drinksByType[$type_name][]= $drink;
+            }
+            return $this->twig->render('Drink/index.html.twig', ['drinksByType' => $drinksByType]);
         }
-        return $this->twig->render('Drink/index.html.twig', ['drinksByType' => $drinksByType]);
     }
-}
