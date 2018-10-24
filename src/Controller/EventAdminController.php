b640a62fc3d5cb5650108378f3da9dfdda308ebe
Patricia Batjom <patriciabatjom@gmail.com>
2018-10-24 12:21:06 +0200

    ajout des vérifications côté serveur création fonction testInput et check

--- src/Controller/EventAdminController.php
+++ src/Controller/EventAdminController.php
@@ -25,6 +25,42 @@ class EventAdminController extends AbstractController
     {
         return $this->twig->render('EventAdmin/index.html.twig');
     }
+
+    /**
+     * @param $data
+     * @return string
+     */
+    private function testInput($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+
+    private function check()
+    {
+        $err=[];
+// Traitement du formulaire
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Teste si tous les champs sont remplis
+            if (empty($_POST["title"])) {
+                $err[] = "ecrire le titre de l'événement";
+            }
+            if (empty($_POST["date"])) {
+                $err[] = "Ecrire la date";
+            } else {
+                if (!preg_match("/(2\d{3})-(0[0-9]|1[0-2])-([0-3][0-9])/", $_POST["date"])) {
+                    $err[] = "Ecrire la date au format YYYY-MM-DD";
+                }
+            }
+        }
+        return $err;
+    }
+
+
+
     /**
      * Display item creation page
      *
@@ -36,15 +72,15 @@ class EventAdminController extends AbstractController
     public function add()
     {
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && empty(self::check())) {
             $eventManager = new EventManager($this->getPdo());
             $event = new Event();
-            $event->setTitle($_POST['title']);
-            $event->setDate($_POST['date']);
+            $event->setTitle(self::testInput($_POST['title']));
+            $event->setDate(self::testInput($_POST['date']));
             $event->setComment($_POST['comment']);
             $id = $eventManager->insert($event);
         }
 
-        return $this->twig->render('EventAdmin/add.html.twig');
+        return $this->twig->render('EventAdmin/add.html.twig', ['errors'=> self::check()]);
     }
 }
