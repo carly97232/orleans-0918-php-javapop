a8f04ba98625b088f97c89af5b5a5b4da8538938
HOARAU Mathieu <Mathieuhoarau@laposte.net>
2018-10-26 14:06:22 +0200

    change routing and view

--- app/routing.php
+++ app/routing.php
@@ -40,7 +40,7 @@ $routes = [
 
     'GalleryAdmin' => [
         ['index', '/admin/galleryAdmin', 'GET'],
-        ['delete', '/admin/galleryAdmin/', 'POST'],
+        ['delete', '/admin/galleryAdmin', 'POST'],
     ],
 ];
 
