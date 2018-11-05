c01f1ce25a5e06da58fcb9af78b40f9ba65f5500
HOARAU Mathieu <Mathieuhoarau@laposte.net>
2018-11-05 17:41:48 +0100

    ajout des erreurs

--- src/Controller/GalleryAdminController.php
+++ src/Controller/GalleryAdminController.php
@@ -18,6 +18,8 @@ use Model\PictureManager;
  */
 class GalleryAdminController extends AbstractController
 {
+    const AUTH_EXT = array('.png', '.gif', '.jpg', '.jpeg');
+
     /**
      * @return string
      * @throws \Twig_Error_Loader
@@ -32,53 +34,53 @@ class GalleryAdminController extends AbstractController
         return $this->twig->render('GalleryAdmin/index.html.twig', ['pictures' => $pictures]);
     }
 
-    public function addPic()
+    private function pictureVerification()
     {
-
-        $maxSize = 5000000;
-        $extensions = array('.png', '.gif', '.jpg', '.jpeg');
-
+        $errors = [];
         if (isset($_POST['submit'])) {
             if (count($_FILES['upload']['name']) > 0) {
                 for ($i = 0; $i < count($_FILES['upload']['name']); $i++) {
-                    $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
                     $size = filesize($_FILES['upload']['tmp_name'][$i]);
+
                     $extension = strrchr($_FILES['upload']['name'][$i], '.');
 
-                    if (!in_array($extension, $extensions)) {
-                        $errors['name'] = 'Vous devez uploader un fichier de type ' . implode(', ', $extensions);
-                        return $this->twig->render('GalleryAdmin/add.html.twig', ['error' => $errors]);
+                    if (!in_array($extension, self::AUTH_EXT)) {
+                        $errors['name'] = 'Vous devez uploader un fichier de type ' . implode(', ', self::AUTH_EXT);
                     }
 
-                    if ($size > $maxSize) {
+                    if (($size > 2097152) or ($size == 0)) {
                         $errors['name'] = 'Le fichier est trop gros...';
-                        return $this->twig->render('GalleryAdmin/add.html.twig', ['error' => $errors]);
                     }
+                }
+            }
+        }
+        return $errors;
+    }
 
-                    if ($tmpFilePath != "") {
-                        $shortName = $_FILES['upload']['name'][$i];
-                        //save the url and the file
+    public function addPic()
+    {
+        $errors = $this->pictureVerification();
 
-                        $filePath = "assets/images/gallery/" . 'image' . date('d-m-Y-H-i-s') . '-' . $_FILES['upload']['name'][$i];
-                        if (!isset($errors)) {
-                            if (move_uploaded_file($tmpFilePath, $filePath)) {
-                                $files[] = $shortName;
-
-                                $imgName = 'image' . date('d-m-Y-H-i-s') . '-' . $_FILES['upload']['name'][$i];
-                                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                                    $pictureManager = new PictureManager($this->getPdo());
-                                    $picture = new Picture();
-                                    $picture->setImgName($imgName);
-                                    $id = $pictureManager->insert($picture);
-                                    header('Location:/admin/galleryAdmin');
-                                }
-                            }
+        if (empty($errors)) {
+            if (isset($_POST['submit'])) {
+                for ($i = 0; $i < count($_FILES['upload']['name']); $i++) {
+                    $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+                    $shortName = $_FILES['upload']['name'][$i];
+                    $filePath = "assets/images/gallery/" . 'image' . date('d-m-Y-H-i-s')
+                        . '-' . $_FILES['upload']['name'][$i];
+                    if (move_uploaded_file($tmpFilePath, $filePath)) {
+                        $imgName = 'image' . date('d-m-Y-H-i-s') . '-' . $shortName;
+                        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                            $pictureManager = new PictureManager($this->getPdo());
+                            $picture = new Picture();
+                            $picture->setImgName($imgName);
+                            $id = $pictureManager->insert($picture);
+                            header('Location:/admin/galleryAdmin');
                         }
                     }
                 }
             }
         }
-
-        return $this->twig->render('GalleryAdmin/add.html.twig');
+        return $this->twig->render('GalleryAdmin/add.html.twig', ['errors' => $errors]);
     }
 }
