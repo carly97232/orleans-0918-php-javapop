fdaa7a6489ed2601fcdd5af0ed941344cafa17f2
Patricia Batjom <patriciabatjom@gmail.com>
2018-11-06 17:45:02 +0100

    eventAdminController

--- src/Controller/EventAdminController.php
+++ src/Controller/EventAdminController.php
@@ -2,32 +2,25 @@
 /**
  * Created by PhpStorm.
  * User: patricia
- * Date: 23/10/18
- * Time: 16:18
-
+ * Date: 25/10/18
+ * Time: 10:19
  */
 
 namespace Controller;
 
-use Model\Event;
 use Model\EventManager;
+use Model\Event;
 use Filter\Text;
 
 class EventAdminController extends AbstractController
 {
-
-
-    /**
-     * @return string
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
-     */
     public function index()
     {
-        return $this->twig->render('EventAdmin/index.html.twig');
-    }
+        $eventManager = new EventManager($this->getPdo());
+        $events = $eventManager->selectAll('date', 'DESC');
 
+        return $this->twig->render('EventAdmin/index.html.twig', ['events' => $events]);
+    }
 
     /**
      * @param array $userData
@@ -36,7 +29,6 @@ class EventAdminController extends AbstractController
     private function check(array $userData):array
     {
         $errorsForm = [];
-
         if (empty($userData['title'])) {
             $errorsForm[] = "Ecrire le titre de l'événement";
         }
@@ -49,53 +41,6 @@ class EventAdminController extends AbstractController
             }
         }
         return $errorsForm;
-    }
-
-
-    /**
-     * Display item creation page
-     *
-     * @return string
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
-     */
-    public function add()
-    {
-
-        $errors = $userData = [];
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $userData = $_POST;
-            $textFilter = new Text();
-            $textFilter->setTexts($userData);
-            $userData = $textFilter->filter();
-
-            $errors = $this->check($userData);
-            if (empty($errors)) {
-                $eventManager = new EventManager($this->getPdo());
-                $event = new Event();
-                $event->setTitle($userData['title']);
-                $event->setDate(new \DateTime($userData['date']));
-                $event->setComment($userData['comment']);
-                $id = $eventManager->insert($event);
-                header('Location: /admin/eventAdmin/add');
-            }
-        }
-        return $this->twig->render('EventAdmin/add.html.twig', ['errors' => $errors]);
-    }
-
-
-    public function delete()
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (isset($_POST['deleteEvent'])) {
-                $eventManager = new EventManager($this->getPdo());
-                $eventManager->delete($_POST['deleteEvent']);
-                header('location:/admin/eventAdmin/index');
-                exit();
-            }
-        }
     }
 
     /**
@@ -119,9 +64,9 @@ class EventAdminController extends AbstractController
             $userData = $textFilter->filter();
             $errors = $this->check($userData);
 
-            if (empty($errors)) {
+            if (isset($_POST['validate'])) {
                 $event->setTitle($userData['title']);
-                $event->setDate(\DateTime::createFromFormat('Y-m-d', $userData['date']));
+                $event->setDate($userData['date']);
                 $event->setComment($userData['comment']);
                 $eventManager->update($event);
             }
